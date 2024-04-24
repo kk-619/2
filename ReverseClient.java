109ba664685c352ba0b55f9decc5a6ffc9ec4065
kk-619 <106342694+kk-619@users.noreply.github.com>
2024-04-24 13:37:57 +0530

    Update ReverseClient.java

--- ReverseClient.java
+++ ReverseClient.java
@@ -39,3 +39,20 @@ class ReverseClient
         }
     }
 }
+
+/*
+ * cmds:
+ * sudo apt-get remove openjdk*
+ * sudo apt update
+ * sudo apt install openjdk-8-jdk openjdk-8-jre
+ * Terminal1:
+ * 
+ * idlj -fall ReverseModule.idl
+ * javac .java ReverseModule/.java
+ * orbd -ORBInitialPort 1056&
+ * java ReverseServer -ORBInitiaPort 1056&
+ * Reverse Object Created
+ * 
+ * Terminal2:
+ * java ReverseClient -ORBInitialPort 1056 -ORBInitialHost localhost
+ */
